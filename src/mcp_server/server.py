86c478802e2d1ecea7439bcb5fb63481993aaad7
Fumio <foobarbazfred@gmail.com>
2025-10-05 00:24:39 +0900

    Update server.py

--- src/mcp_server/server.py
+++ src/mcp_server/server.py
@@ -6,6 +6,7 @@
 # start with uv run server.py
 #
 
+import paho.mqtt.client as mqtt
 from mcp.server.lowlevel import Server
 import mcp.types as types
 import uvicorn
@@ -15,6 +16,44 @@ from starlette.routing import Route, Mount
 from mcp.server.sse import SseServerTransport
 from starlette.requests import Request
 import json
+
+
+#
+# setup for MQTT
+#
+
+# define of MQTT
+
+MQTT_BROKER = 'broker.emqx.io'
+MQTT_PORT = 1883
+MQTT_TOPIC = "req/maqueen01/control"
+
+# global variable
+mqtt_client = None
+
+GO = json.dumps({'control' : {'device':'car', 'set' : 'go'}})
+BACK = json.dumps({'control' : {'device':'car', 'set' : 'back'}})
+SPIN_L = json.dumps({'control' : {'device':'car', 'set' : 'spin_left'}})
+SPIN_R = json.dumps({'control' : {'device':'car', 'set' : 'spin_right'}})
+STOP = json.dumps({'control' : {'device':'car', 'set' : 'stop'}})
+
+def init_mqtt():
+
+    global mqtt_client
+    mqtt_client = mqtt.Client()
+    
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    mqtt_client.loop_start()
+    print(f"MQTT connected to {MQTT_BROKER}:{MQTT_PORT}")
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+def on_message(client, userdata, msg):
+    print(f"Received: {msg.topic} {msg.payload}")
 
 app = Server("maqueen")
 
@@ -40,6 +79,8 @@ async def call_tool(name: str, arguments: dict) -> list[types.ContentBlock]:
             result_text = "Moving forward continuously (until stopped)"
         else:
             result_text = f"Moving forward for {duration} seconds"
+        msg = GO
+        result = client.publish(MQTT_TOPIC, msg , qos=0)
         print(f"Returning: {result_text}")
         return [types.TextContent(type="text", text=result_text)]
     
@@ -71,6 +112,8 @@ async def call_tool(name: str, arguments: dict) -> list[types.ContentBlock]:
         return [types.TextContent(type="text", text=result_text)]
     
     elif name == "stop":
+        msg = STOP
+        result = client.publish(MQTT_TOPIC, msg , qos=0)
         result_text = "Stopping Maqueen"
         print(f"Returning: {result_text}")
         return [types.TextContent(type="text", text=result_text)]
@@ -250,4 +293,4 @@ starlette_app = Starlette(
 
 if __name__ == "__main__":
     uvicorn.run(starlette_app, host="0.0.0.0", port=1885)
-)
+
